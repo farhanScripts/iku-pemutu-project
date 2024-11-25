9c34195ad52b91e1f2deb57a0ded6f81cd2fd160
Imam <imamzuhdimuzakkiy1@gmail.com>
2024-11-25 12:37:52 +0700

    Menambahkan livewire pemutu prodi & naming route

--- routes/web.php
+++ routes/web.php
@@ -16,61 +16,71 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function(){
+        Route::get('/telusur-data', function() {
+            return view('admin.telusur-data.index');
+        })->name('telusur-data')->middleware('role:admin');
+
+        Route::get('/telusur-data/iku3', function() {
+            return view('admin.telusur-data.IKU3.dsn_penelitian');
+        })->name('telusur-data.iku3')->middleware('role:admin');
+    });
 });
 
-Route::prefix('akreditasi-prodi')->group(function() {
+Route::prefix('akreditasi-prodi')->name('akreditasi-prodi.')->group(function() {
     Route::get('daftar-prodi', function() {
-        return view('#');
-    });
+        return view('dashboard');
+    })->name('daftar-prodi');
 
-    Route::prefix('daftar-prodi')->group(function() {
+    Route::prefix('daftar-prodi')->name('daftar-prodi.')->group(function() {
         Route::get('s1-{namaJurusan}', function($namaJurusan) {
             return view('#');
-        });
+        })->name('s1');
         Route::get('s2-{namaJurusan}', function($namaJurusan) {
             return view('#');
-        });
+        })->name('s2');
         Route::get('s3-{namaJurusan}', function($namaJurusan) {
             return view('#');
-        });
+        })->name('s3');
         Route::get('d3-{namaJurusan}', function($namaJurusan) {
             return view('#');
-        });
+        })->name('d3');
 
-        Route::prefix('s1-{namaJurusan}')->group(function() {
+        Route::prefix('s1-{namaJurusan}')->name('s1.')->group(function() {
             Route::get('metrik', function($namaJurusan) {
                 return view('#');
-            });
+            })->name('metrik');
             Route::get('metrik/detail/{id}', function($namaJurusan, $id) {
                 return view('#');
-            });
+            })->name('metrik-detail');
         });
 
-        Route::prefix('s2-{namaJurusan}')->group(function() {
+        Route::prefix('s2-{namaJurusan}')->name('s2.')->group(function() {
             Route::get('metrik', function($namaJurusan) {
                 return view('#');
-            });
+            })->name('metrik');
             Route::get('metrik/detail/{id}', function($namaJurusan, $id) {
                 return view('#');
-            });
+            })->name('metrik-detail');
         });
 
-        Route::prefix('s3-{namaJurusan}')->group(function() {
+        Route::prefix('s3-{namaJurusan}')->name('s3.')->group(function() {
             Route::get('metrik', function($namaJurusan) {
                 return view('#');
-            });
+            })->name('metrik');
             Route::get('metrik/detail/{id}', function($namaJurusan, $id) {
                 return view('#');
-            });
+            })->name('metrik-detail');
         });
 
-        Route::prefix('d3-{namaJurusan}')->group(function() {
+        Route::prefix('d3-{namaJurusan}')->name('d3.')->group(function() {
             Route::get('metrik', function($namaJurusan) {
                 return view('#');
-            });
+            })->name('metrik');
             Route::get('metrik/detail/{id}', function($namaJurusan, $id) {
                 return view('#');
-            });
+            })->name('metrik-detail');
         });
     });
 });
